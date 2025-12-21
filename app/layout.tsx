c41a6af374c16f73d648a583810d9881dd9104e0
kuekhaoyang <kuekhaoyang@Gmail.com>
2025-12-21 13:34:18 +0800

    feat: Configure layout to use edge runtime.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClientProviders } from "@/components/ClientProviders";
 
+export const runtime = 'edge';
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
